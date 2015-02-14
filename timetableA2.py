4de1d03d0ebf318b609a2844f1ed9ffd955a4e66
nitor <brett.mulligan@gmail.com>
2015-02-14 09:14:20 -0800

    Random assignments, IO improvements, and exam week display

--- timetableA2.py
+++ timetableA2.py
@@ -15,6 +15,8 @@ import sys
 import os
 import copy
 
+ARGS = 5
+
 VERBOSE_LOADING = False
 VERBOSE_TESTING = True
 
@@ -37,15 +39,24 @@ class ExamWeek:
     def __init__(self, rmcap, num_exams):
         self.room_cap = rmcap
         self.timeslots = num_exams
+        
+    def display(self, courses):
+        data = '--- Exam Week ---\n' + 'Room capacity: ' + str(self.room_cap) + '\nTotal Exams: ' + str(self.timeslots) + '\n'
+        
+        for x in range(1, self.timeslots + 1):
+            data += '\n' + str(x)                        # TODO: plus any courses in this timeslot
+            data += str(get_course_with_timeslot(courses, x))
+        data += '\n'
+        return data
 
 class Student:
 
     def __init__(self, name):
         self.stu_id = name
-        self.courses = ()
+        self.courses = []
         
-    def add_course(crs):
-        self.courses = tuple(list(self.courses).append(crs))
+    def add_course(self, crs):
+        self.courses.append(crs)
 
 class Course:
 
@@ -55,6 +66,12 @@ class Course:
         self.enrolled = enrollment
         self.timeslot = 0
 
+def get_course_with_timeslot(courses, slot):
+    return [course.crs_id for course in courses if course.timeslot == slot]
+
+def assign_random_timeslot(courses, maximum):
+    for course in courses:
+        course.timeslot = random.randint(1,maximum)
     
 def read_crs_file(filename):
     courses = []
@@ -81,41 +98,48 @@ def read_crs_file(filename):
             
         print ''
         
-        """
-        line = line.strip()                                         # strip whitespace
+    return courses, room_cap, timeslots
 
-        if len(line) > 1 and line[0] not in IGNORE:                 # ignore comment lines and lines of only 1 char, process everything else                    
-            # print line
-
-            if line[0] == 'p':                                      # first line, grab data
-                data = line.split()
-                self.num_variables = int(data[2])
-                self.num_clauses = int(data[3])
-
-            else:                                                   # clauses
-                temp_clauses.append(self.transform_clause(line.split()))
-        """
+def total_timeslots():
+    return 7
+    
+def total_student_cost():
+    return 133.75
         
 def read_stu_file(filename):
+    students = []
+    
     f = open(filename, 'r')
     if f == None:
         print "TT Error: read_stu_file could not open file:", filename 
     
     else:
+        stud_count = 1
         for line in f:
             print line
+            new_student = Student(stud_count)
+            courses = line.split()
+            for course in courses:
+                new_student.add_course(course)
+            students.append(new_student)
+            stud_count += 1
             
         print ''
+        
+    return students
 
 def print_solution(sol):
     printable = ''
     
-    printable = printable + '7\t133.75'
+    printable = printable + str(total_timeslots()) + '\t' + str(total_student_cost())
 
     for crs in sol:
-        exam_day = crs.timeslot // TIMESLOTS_PER_DAY
-        exam_timeslot = crs.timeslot % TIMESLOTS_PER_DAY
-        printable += '\n' + crs.crs_id + '\t' + str(exam_day) + '\t' + str(exam_timeslot)
+        exam_day = (crs.timeslot // TIMESLOTS_PER_DAY) + 1
+        exam_timeslot = (crs.timeslot % TIMESLOTS_PER_DAY)
+        if (exam_timeslot == 0):
+            exam_timeslot = TIMESLOTS_PER_DAY
+            exam_day -= 1
+        printable += '\n' + crs.crs_id + '\t' + str(exam_day) + '\t' + str(exam_timeslot) + '\t' + str(crs.timeslot)
     
     return printable
         
@@ -124,19 +148,31 @@ def test_instance(crsfn, stufn):
     if VERBOSE_TESTING:
         print 'Testing...', crsfn, 'and', stufn, "@", time.asctime()
 
-    read_crs_file(crsfn)
-    read_stu_file(stufn)
+    courses, room_cap, num_exams = read_crs_file(crsfn)
+    students = read_stu_file(stufn)
     
-    sol = [Course('001', 10) for x in range(0,10)]
-    print print_solution(sol)
+    assign_random_timeslot(courses, num_exams)
+    
+    ### Check constraints
+    
+    
+    print ExamWeek(room_cap, num_exams).display(courses)
+    print print_solution(courses)
         
     print ''
 
     return
 
 if __name__ == "__main__":
-    
-    test_instance(crsfile, stufile)
+
+    if len(sys.argv) != ARGS:
+        print 'Usage: ', sys.argv[0], '<coursefile> <studentfile> <solutionfile> <objectivefunction (0 or 1)>'
+    else:
+        crsfile = sys.argv[1]
+        stufile = sys.argv[2]
+        solfile = sys.argv[3]
+        obj_function = sys.argv[4]
+        test_instance(crsfile, stufile)
 
     print "Testing complete!"
 
