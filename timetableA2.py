9dbb54671ef88689372af15e4a33eed1af611267
nitor <brett.mulligan@gmail.com>
2015-02-13 17:08:30 -0800

    IO prints correctly

--- timetableA2.py
+++ timetableA2.py
@@ -18,50 +18,50 @@ import copy
 VERBOSE_LOADING = False
 VERBOSE_TESTING = True
 
-subdir = './'
-crsfile = "instance1.crs"
-stufile = "instance1.stu"
-solfile = "instance1.sol"
 
 DEC_PRECISION = 5
 PERCENT_DIGITS = 2
 EOL = '\n'
 
 
+subdir = './'
+crsfile = "instance1.crs"                   # CHANGE BEFORE TURN-IN
+stufile = "instance1.stu"
+solfile = "instance1.sol"
+
+TIMESLOTS_PER_DAY = 5
+
+
+class ExamWeek:
+
+    def __init__(self, rmcap, num_exams):
+        self.room_cap = rmcap
+        self.timeslots = num_exams
+
 class Student:
 
-    def __init__(name):
+    def __init__(self, name):
         self.stu_id = name
         self.courses = ()
+        
+    def add_course(crs):
+        self.courses = tuple(list(self.courses).append(crs))
 
 class Course:
 
-    def __init__(name):
+    def __init__(self, name, enrollment):
         self.crs_id = name
         self.students = ()
-        self.enrolled = len(self.students)
-    
-
-def test_instance(crsfn, stufn):
-    
-    if VERBOSE_TESTING:
-        print 'Testing...', crsfn, 'and', stufn, "@", time.asctime()
-
-    read_crs_file(crsfn)
-    read_stu_file(stufn)
-        
-    print ''
-
-    return
+        self.enrolled = enrollment
+        self.timeslot = 0
 
     
 def read_crs_file(filename):
+    courses = []
+    
     f = open(filename, 'r')
     if f == None:
         print "TT Error: read_crs_file could not open file:", filename 
-    
-        
-    
     else:
         data = f.readline()
         print data
@@ -70,11 +70,14 @@ def read_crs_file(filename):
         room_cap = int(data[0])
         timeslots = int(data[1])
         
-        print 'Room cap:', room_cap
+        print 'Room Capacity:', room_cap
         print 'Timeslots:', timeslots
+        
+        print ''
         
         for line in f:
             print line
+            courses.append(Course(line.split('\t')[0], line.split('\t')[1]))
             
         print ''
         
@@ -104,7 +107,32 @@ def read_stu_file(filename):
             
         print ''
 
+def print_solution(sol):
+    printable = ''
+    
+    printable = printable + '7\t133.75'
 
+    for crs in sol:
+        exam_day = crs.timeslot // TIMESLOTS_PER_DAY
+        exam_timeslot = crs.timeslot % TIMESLOTS_PER_DAY
+        printable += '\n' + crs.crs_id + '\t' + str(exam_day) + '\t' + str(exam_timeslot)
+    
+    return printable
+        
+def test_instance(crsfn, stufn):
+    
+    if VERBOSE_TESTING:
+        print 'Testing...', crsfn, 'and', stufn, "@", time.asctime()
+
+    read_crs_file(crsfn)
+    read_stu_file(stufn)
+    
+    sol = [Course('001', 10) for x in range(0,10)]
+    print print_solution(sol)
+        
+    print ''
+
+    return
 
 if __name__ == "__main__":
     
